8fbad0744c637df143c6e772ddff867fd1985ad0
Henry Wilder <henrythepony@gmail.com>
2022-10-25 18:08:09 -0400

    Added file reading

--- thewall/src/index.ts
+++ thewall/src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello world!');
+import path from "path";
+import fs from "fs";
+
+interface IVec3 {
+    x: number;
+    y: number;
+    z: number;
+}
+interface IBody {
+    radius: number;
+    startPos: IVec3;
+}
+
+const bodiesFilePath: string = path.resolve("./src/config/bodies.json");
+const bodies: Array<IBody> = JSON.parse(fs.readFileSync(bodiesFilePath,"utf8"));
+
+console.log(bodies[0].radius);
