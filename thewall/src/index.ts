029e4afb9f956adad45276eea2c3d0fa988160f1
Henry Wilder <henrythepony@gmail.com>
2022-10-28 20:31:16 -0400

    Added additional sub-program

--- thewall/src/index.ts
+++ thewall/src/index.ts
@@ -5,12 +5,14 @@ import { logStateAggregates } from "./util/aggregateStates";
 
 (async () => {
     try {
+        console.log("Body data:");
         try {
             logBodyData(0);
         } catch (error) {
             console.error(error);
         }
 
+        console.log("\nStatus aggregates:");
         try {
             // Arrays
             const keyList: string[] = [
@@ -31,8 +33,20 @@ import { logStateAggregates } from "./util/aggregateStates";
                 StateEnum.active,
             ];
 
-            console.log("Status aggregates:");
             logStateAggregates(keyList, valueList, "a,b,k,@hibernating,n");
+        } catch (error) {
+            console.error(error);
+        }
+
+        console.log("\nThingly potatoness:");
+        try {
+            const isItAPotato = (thing: string) => thing == "potato";
+
+            let allTheThings = ["potato", "not a potato"];
+
+            let thinglyPotatoness = allTheThings.map(isItAPotato);
+
+            allTheThings.forEach((thing, index) => console.log(`"${thing}" ${thinglyPotatoness[index] ? "is" : "is not" } a potato`));
         } catch (error) {
             console.error(error);
         }
