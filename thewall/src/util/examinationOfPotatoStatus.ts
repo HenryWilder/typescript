16abca4d66acc70377866edf492f79f95877fa6b
Henry Wilder <henrythepony@gmail.com>
2022-10-28 20:39:22 -0400

    Formatted

--- thewall/src/util/examinationOfPotatoStatus.ts
+++ thewall/src/util/examinationOfPotatoStatus.ts
@@ -1,6 +1,7 @@
 export const logThinglyPotatoness = (allTheThings: string[]) => {
     let thinglyPotatoness = allTheThings.map(isItAPotato);
-    allTheThings.forEach((thing, index) => console.log(`"${thing}" ${thinglyPotatoness[index] ? "is" : "is not"} a potato`));
+    allTheThings.forEach((thing, index) =>
+        console.log(`"${thing}" ${thinglyPotatoness[index] ? "is" : "is not"} a potato`));
 };
 
 const isItAPotato = (thing: string) => thing == "potato";
