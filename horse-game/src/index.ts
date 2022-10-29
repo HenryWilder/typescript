8b7fea53d6334e8136b8d1a37ae85e37bca6306f
Henry Wilder <henrythepony@gmail.com>
2022-10-29 09:43:47 -0400

    Basic clear console and wait

--- horse-game/src/index.ts
+++ horse-game/src/index.ts
@@ -1,3 +1,10 @@
-(() => {
-    console.log("Hello world");
+const sleep = (milliseconds: number) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+(async () => {
+    console.log("\x1b[uHello");
+    await sleep(1000);
+    console.clear();
+    console.log("\x1b[uEEEEE");
 })();
